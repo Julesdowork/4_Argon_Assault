7f4d635d29799af547fc61d73244a1baa22894fb
Julian McNeill <julesdowork@gmail.com>
2018-07-16 19:28:23 -0400

    Understanding Roll, Pitch, & Yaw

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,22 +5,30 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 15f;
     [SerializeField] float xLimit = 3f;
+    [SerializeField] float yUpperLimit = 2f;
+    [SerializeField] float yLowerLimit = -2f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xLimit, xLimit);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yLowerLimit, yUpperLimit);
 
         transform.localPosition = new Vector3(clampedXPos,
-            transform.localPosition.y, transform.localPosition.z);
+            clampedYPos, transform.localPosition.z);
 	}
 }
