071f839f003e2abe1654acade23cd1bb79d5bfd3
Julian McNeill <julesdowork@gmail.com>
2018-07-17 19:10:47 -0400

    Enemy Health System

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 10;
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
 
@@ -19,16 +20,30 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        GameObject explosion =  Instantiate(deathFX, transform.position,
-            Quaternion.identity);
-        explosion.transform.parent = parent;
-        Destroy(gameObject);
-        scoreBoard.ScoreHit(scorePerHit);
+        ProcessHits();
     }
 
     void AddBoxCollider()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
+    }
+
+    void KillEnemy()
+    {
+        GameObject explosion = Instantiate(deathFX, transform.position,
+                    Quaternion.identity);
+        explosion.transform.parent = parent;
+        Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
+    }
+
+    void ProcessHits()
+    {
+        hits = hits - 1;
+        if (hits <= 0)
+        {
+            KillEnemy();
+        }
     }
 }
