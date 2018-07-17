c7224f06ddbc37f9615e4e646c2a1ad1c4ac14ee
Julian McNeill <julesdowork@gmail.com>
2018-07-17 14:01:53 -0400

    Detecting Particle Collisions

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        print("Particles collided with enemy " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
