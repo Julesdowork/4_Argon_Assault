19440e3e9aa0c2c6e7ce137154cbb41a2ba3f7b3
Julian McNeill <julesdowork@gmail.com>
2018-07-17 15:58:30 -0400

    Introducing Encapsulation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] int scorePerHit = 12;
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
 
+    ScoreBoard scoreBoard;
+
     void Start()
     {
-        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
-        boxCollider.isTrigger = false;
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     void OnParticleCollision(GameObject other)
@@ -20,5 +23,12 @@ public class Enemy : MonoBehaviour
             Quaternion.identity);
         explosion.transform.parent = parent;
         Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
+    }
+
+    void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
     }
 }
